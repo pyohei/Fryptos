fcc8c04f4139917d614f8b4d4c63881235da6599
pyohei <mukaishohei76@gmail.com>
2015-08-15 00:55:04 +0900

    add functions to move

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ This module anable change the anchers.
 import ConfigParser
 import glob
 import os
+import shutil
 import filename
 from anchor import Anchor
 
@@ -18,9 +19,13 @@ def main(conf_path='conf/conf.ini'):
     conf.read(conf_path)
     anchor = Anchor('text')
     for org_f in __read_files(conf.get('directory', 'file_top')):
-        cur_f = anchor.has(org_f)
-        enc_f = __encrypt_file(org_f)
+        cur_f = anchor.load_cur(org_f)
+        enc_f = __make_public_dir(conf.get('directory', 'public_dir',
+                                           __encrypt_file(org_f, anchor)))
+        __move(org_f, enc_f)
         anchor.change(cur_f, enc_f)
+        if os.path.exists(cur_f):
+            __delete(cur_f)
 
 
 def __read_files(file_path):
@@ -36,3 +41,16 @@ def __encrypt_file(fname, anchor):
     if anchor.haq(f):
         __encrypt_file(filename)
     return f
+
+
+def __make_public_dir(public_dir, file_path):
+    return os.path.join(public_dir, file_path)
+
+
+def __move(org_f, enc_f):
+    os.makedirs(os.path.join(enc_f.split('/')[0:-1]))
+    shutil.copy(org_f, enc_f)
+
+
+def __delete(cur_f):
+    shutil.rmtree(cur_f.split('/')[0])
