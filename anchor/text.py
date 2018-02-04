d031d32e6fd27223823b25b818f212e507d9f6ad
pyohei <mukaishohei76@gmail.com>
2018-02-04 22:18:30 +0900

    Add changing code.

--- anchor/text.py
+++ anchor/text.py
@@ -3,6 +3,7 @@
 Supply text operations.
 """
 
+import csv
 import os
 import pickle
 
@@ -11,18 +12,31 @@ class Text(object):
 
     def __init__(self, settings):
         self.file_path = settings.get('ancfile_path', 'anc.txt')
+        # New saving file(tsv).
+        self.path = settings.get('ancfile_path', 'anc.tsv')
+        print self.path
         self.anchors = {}
-        self.__load()
+        # New anchor
+        self.new_anchors = {}
+        self._load()
         self.encrypted_words = []
-        self.__load_encrypt_words()
+        self._load_encrypt_words()
 
-    def __load(self):
+    def _load(self):
         if not os.path.exists(self.file_path):
             return
         with open(self.file_path, 'r') as f:
             self.anchors = pickle.load(f)
+        with open(self.path + '.tsv', 'r') as ff:
+            self.new_anchors = csv.DictReader(ff)
+            a = {}
+            for n in self.new_anchors:
+                a[n['source']] = n['destination']
+        print "]]]]"
+        print self.anchors == a
+        print "]]]]"
 
-    def __load_encrypt_words(self):
+    def _load_encrypt_words(self):
         words = []
         for w in self.anchors.values():
             words += w.split('/')
@@ -37,16 +51,22 @@ class Text(object):
     def change(self, org_file, enc_file):
         self.anchors[org_file] = enc_file
         try:
-            self.__save()
-            self.__load()
+            self._save()
+            self._load()
         except Exception as e:
             print e
             raise
 
-    def __save(self):
+    def _save(self):
         tmp_file = self.file_path + '.org'
         with open(tmp_file, 'w') as f:
             pickle.dump(self.anchors, f)
+        with open(self.path + '.tsv', 'w') as ff:
+            w = csv.DictWriter(ff, fieldnames=['source', 'destination'])
+            w.writeheader()
+            for k, v in self.anchors.items():
+                w.writerow({'source': k, 'destination': v})
+
         self.__change_file(tmp_file)
 
     def __change_file(self, tmp_file):
