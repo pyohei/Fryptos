c8a42cb2eaea39cc4598c0b40c41f64c85a2a7c1
pyohei <mukaishohei76@gmail.com>
2018-03-05 22:48:34 +0900

    Edit setup

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 """Set up script"""
 from distutils.core import setup
 
-setup(name="shuffler",
-        version='1.0.0',
-        py_modules=[''])
+setup(name="kakurenbo",
+      version='1.0.0',
+      description='Encrypt files.',
+      author='Shohei Mukai',
+      author_email='mukaishohei76@gmail.com',
+      py_modules=['kakurenbo'],
+      )
 
