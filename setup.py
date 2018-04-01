27fc65f332d13081d9f05745532df2429db48f39
pyohei <mukaishohei76@gmail.com>
2018-04-01 21:58:10 +0900

    Fix setup py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 """Set up script"""
 from distutils.core import setup
 
-setup(name="kakurenbo",
+setup(name="fryptos",
       version='1.0.0',
       description='Encrypt files.',
       author='Shohei Mukai',
       author_email='mukaishohei76@gmail.com',
-      py_modules=['kakurenbo'],
+      py_modules=['main'],
       )
 
