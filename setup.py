955ed30f2b7e946bbc734b550e29df3f1e6f54dd
pyohei <mukaishohei76@gmail.com>
2018-04-06 07:25:28 +0900

    Add setup py columns.

--- setup.py
+++ setup.py
@@ -4,8 +4,19 @@ from distutils.core import setup
 setup(name="fryptos",
       version='0.0.1',
       description='Encrypt files.',
+      # long_description="",
+      # classifiers=[],
+      keywords='encrypt'
       author='Shohei Mukai',
       author_email='mukaishohei76@gmail.com',
       py_modules=['main'],
+      packages=['fryptos'],
+      entry_points={
+          'console_scripts': [
+              ],
+          },
+      licence='MIT',
+      # install_requires=[],
+      # test_suite=''
       )
 
