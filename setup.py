a2df544d8894da188ad7f0781b6e891de038ac6e
pyohei <mukaishohei76@gmail.com>
2018-05-21 20:31:02 +0900

    Raise version.

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 """Set up script"""
 from setuptools import setup
+import os
 
 def _create_long_desc():
     """Create long description and README formatted with rst."""
     _long_desc = ''
-    with open('README.rst', 'r') as rf:
-        return rf.read()
+    if os.path.isfile('README.md'):
+        with open('README.md', 'r') as rf:
+            return rf.read()
+    if os.path.isfile('README.rst'):
+        with open('README.rst', 'r') as rf:
+            return rf.read()
 long_desc = _create_long_desc()
 
 setup(name="fryptos",
-      version='0.0.7',
+      version='0.0.8',
       description='Encrypt files.',
       long_description=long_desc,
       long_description_content_type='text/markdown',
@@ -22,7 +27,7 @@ setup(name="fryptos",
       author='Shohei Mukai',
       author_email='mukaishohei76@gmail.com',
       url = 'https://github.com/pyohei/Fryptos',
-      packages=['fryptos'],
+      packages=['fryptos', 'fryptos.anchor'],
       entry_points={
           'console_scripts': [
               'fryptos = fryptos.main:execute'],
